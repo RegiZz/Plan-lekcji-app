c89b782cfc110da4847811a4e85af9925b9cc380
RegiZz <bartek.mendrzycki@o2.pl>
2024-09-18 20:32:43 +0200

    Utworzenie projektu

---
+++ modules/planGet.js
@@ -0,0 +1,29 @@
+const Librus = require("librus-api")
+
+let journal = new Librus();
+
+let login = "9336680u";
+let password = "Aureola12122006";
+let date = new Date;
+
+const subjects = [];
+
+let from = "2024-09-16"
+let to = "2024-09-20"
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+ 
+journal.authorize(login,password).then(async function(){
+    journal.calendar.getTimetable(from, to).then((data) => {
+        for(let i = 1; i<=9; i++){
+            if(data.table.Monday[i] !== null){
+                
+                subjects.push(data.table.Monday[i].title)
+            }
+        }
+        console.log(subjects)
+
+    })
+})
