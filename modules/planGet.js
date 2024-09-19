b7999a6d104471e22694c6b4b4ad9ad2b1dcfe0d
RegiZz <bartek.mendrzycki@o2.pl>
2024-09-19 21:21:47 +0200

    Bierze plan aktualny plan

--- modules/planGet.js
+++ modules/planGet.js
@@ -4,23 +4,40 @@ let journal = new Librus();
 
 let login = "9336680u";
 let password = "Aureola12122006";
-let date = new Date;
 
-const subjects = [];
-
-let from = "2024-09-16"
-let to = "2024-09-20"
-
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+function getCurrentWeekDays() {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const mondayOffset = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
+    const weekDays = [];
+    
+    for (let i = 0; i < 5; i++) {
+        const currentDay = new Date(today);
+        currentDay.setDate(today.getDate() + mondayOffset + i);
+        const formattedDate = currentDay.toISOString().slice(0, 10);
+        weekDays.push(formattedDate);
+    }
+    
+    return weekDays;
 }
- 
+
+const monday = [];
+const tuesday = [];
+const wednesday = [];
+const thursday = [];
+const friday = [];
+let days = getCurrentWeekDays()
+
+let from = days[0]
+let to = days[4]
+const subjects = [monday, tuesday, wednesday, thursday, friday];
 journal.authorize(login,password).then(async function(){
     journal.calendar.getTimetable(from, to).then((data) => {
         for(let i = 1; i<=9; i++){
-            if(data.table.Monday[i] !== null){
-                
-                subjects.push(data.table.Monday[i].title)
+            if(data.table.Monday[i]){
+                monday.push(data.table.Monday[i].title)
+            }else{
+                monday.push(data.table.Monday[i])
             }
         }
         console.log(subjects)
