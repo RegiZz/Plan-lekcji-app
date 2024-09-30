00001bf0af5c30829c099b503388a534fa019cba
RegiZz <bartek.mendrzycki@o2.pl>
2024-09-30 18:31:52 +0200

    zarządzanie oknami alpha

--- App.jsx
+++ App.jsx
@@ -1,36 +1,73 @@
 import React from 'react';
 import 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { View } from 'react-native-web';
+import { ToastAndroid } from 'react-native';
 const plan = require("./modules/planGet.js")
 
+const Stack = createNativeStackNavigator();
 let login;
 let password;
 const subjects = plan.Subjects;
 
-const App = () => {
+function loginScreen({navigation}){
   const [writedLogin, setWritedLogin] = useState('');
   const [writedPass, setWritedPass] = useState('');
 
   const zaloguj = () => {
     login = writedLogin;
     password = writedPass;
+    if(plan.authorizeLibrus(login,password)){
+      plan.authorizeLibrus(login,password)
+      navigation.replace("Main")
+    }
+    else{
+      ToastAndroid("Złe dane logowania!")
+    }
+    
   }
 
-  return (
-    <View>
-        <TextInput
-          placeholder = "Wpisz login"
-          onChangeText={newText => setWritedLogin(newText)}
-        />
-        <TextInput
-          placeholder = "Wpisz haslo"
-          onChangeText={newText => setWritedPass(newText)}
-        />
-        <Button
-          title = "Zaloguj"
-          onPress={zaloguj}
-        />
+  return(
+    <View style={styles.main}>
+      <Text style={styles.title}>Logowanie do Librusa</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Login"
+        value={writedLogin}
+        onChangeText={setWritedLogin}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Hasło"
+        value={writedPass}
+        onChangeText={setWritedPass}
+        secureTextEntry
+      />
+      <Button title="Zaloguj" onPress={zaloguj} />
     </View>
-);
+  );
+}
+
+function mainScreen(){
+  return(
+    <View style={styles.main}>
+      
+    </View>
+  );
+}
+
+
+const App = () => {
+  
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={loginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Main" component={mainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
